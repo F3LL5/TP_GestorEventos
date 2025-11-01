1d53049c6a4f269f42c7799f2c6212aba4ae9db9
F3LL5 <dylanolivera565@gmail.com>
2025-11-01 14:39:00 -0300

    Maquetado
    Rutas, Layout General

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GestorEventos } from './pages/gestor-eventos/gestor-eventos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'gestionEventos', component: GestorEventos },
+
+  { path: '', pathMatch: 'full', redirectTo: "/gestionEventos" },
+  { path: '**', redirectTo: "/gestionEventos" }
+];
---
+++ src/app/pages/gestor-eventos/gestor-eventos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestor-eventos',
+  imports: [],
+  templateUrl: './gestor-eventos.html',
+  styleUrl: './gestor-eventos.css',
+})
+export class GestorEventos {
+
+}
